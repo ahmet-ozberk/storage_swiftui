183b1a756b4ed9c48b5bf4040076d01f434c1369
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2022-07-17 15:47:17 +0300

    Initial Commit

---
+++ StorageCli/StorageCliApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageCliApp.swift
+//  StorageCli
+//
+//  Created by Ahmet OZBERK on 17.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct StorageCliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
